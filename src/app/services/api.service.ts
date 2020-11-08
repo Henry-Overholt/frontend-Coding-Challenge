c8c2f29d426914f42cbbe64be377d01135b6510b
Henry Overholt <henrysoverholt@gmail.com>
2020-11-08 17:12:22 -0500

    working getEnrollees api call

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getAllEnrollees(): Observable<any> {
+    return this.http.get('http://localhost:8080/enrollees');
+  }
 }
