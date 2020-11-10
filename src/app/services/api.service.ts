39e7c15bc5f360293e88ec1ff41677bf0afbaf4d
Henry Overholt <henrysoverholt@gmail.com>
2020-11-09 22:21:31 -0500

    added server file into repo and added media querry basics

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,8 +8,6 @@ import { Observable } from 'rxjs';
 export class ApiService {
   id: string;
   enrolleeToEdit: any;
-  randMillerId: string = '89a0cd05-25fb-4b6e-a8f8-fc2187f690d0';
-  landMillerId: string = 'fe1636a3-4d23-4068-ba56-551fae06e29c';
   constructor(private http: HttpClient) {}
   getAllEnrollees(): Observable<any> {
     return this.http.get('http://localhost:8080/enrollees');
@@ -33,5 +31,4 @@ export class ApiService {
     return this.enrolleeToEdit;
   }
 }
-//Rand Miller is the enrollee that is not working, because the ID isn't in the proper format. It's missing hyphens;
-//I can't fix it from here.
+//Rand Miller is the enrollee that is not working, because the ID isn't in the proper format. It's missing the correct hyphens;
